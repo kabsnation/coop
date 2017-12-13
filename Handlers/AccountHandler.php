811f13ee5843d37592384ac8c97c4174da65180f
kabsnation <cpolidan123@gmail.com>
2017-12-13 19:23:30 +0800

    addCooperative

--- Handlers/AccountHandler.php
+++ Handlers/AccountHandler.php
@@ -8,6 +8,10 @@ class AccountHandler{
 		$result = $con->select($query);
 		return $result;
 	}
+	public function addCooperative($cooperativeName,$address,$telephoneNumber,$emailAddress,$CDA_Reg_No,$Date_of_Reg,$CIN,$respondentName,$respondentContactNumber,$position,$respondentEmailAddress,$noOfBoardOfDirectors,$noOfEmployees,$chairman,$viceChairman,$manager,$secretary,$audit,$treasurer,$electionCommitteeChair,$medAndConciliation,$other_Committee,$dateOfRegularMeeting,$dateOfMonthlyMeeting,$dateOfCommitteeMeeting,$businessEngagedIn,$otherBusiness,$serviceOffered,$totalPaidUp,$beginningPaidUp,$toDatePaidUp,$totalCoopAsset,$beginningAsset,$toDateAsset,$birNumber,$tin,$businessPermitNumber,$cocNumber,$cocDateOfIssue,$certificateOfTaxExemptionNumber,$totalMember,$totalMale,$totalFemale,$numberOfRegular,$numberOfAssociate,$trainingAttendByMember,$trainingAttendByOfficers,$trainingAttendByMgtStaff,$idMembershipComposition,$idCommonBondOfMembership,$cooperativeType,$areaId){
+
+		$query="";
+	}
 
 }
 ?>
