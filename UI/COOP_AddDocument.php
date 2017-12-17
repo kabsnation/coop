63f84fc3d04052a937cf100da880c3a757b3315c
Dherp Cuevas <34502161+dherpcuevas@users.noreply.github.com>
2017-12-17 22:10:02 +0800

    Add Document
     Still in progress

---
+++ UI/COOP_AddDocument.php
@@ -0,0 +1,311 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head >
+    <title>CCDO - Add Document</title>
+
+    <link rel="icon" href="../assets/images/CCDO Logo.png" />
+
+	<!-- Global stylesheets -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+	<link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/core.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/components.css" rel="stylesheet" type="text/css">
+	<link href="assets/css/colors.css" rel="stylesheet" type="text/css">
+	<!-- /global stylesheets -->
+
+	<!-- Core JS files -->
+	<script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
+	<script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
+	<script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
+	<!-- /core JS files -->
+
+	<!-- Theme JS files -->
+	<script type="text/javascript" src="assets/js/plugins/tables/datatables/datatables.min.js"></script>
+	<script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
+
+	<script type="text/javascript" src="assets/js/core/app.js"></script>
+	<script type="text/javascript" src="assets/js/pages/datatables_data_sources.js"></script>
+    <script type="text/javascript" src="assets/js/pages/form_inputs.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/uploaders/fileinput.min.js"></script>
+    <script type="text/javascript" src="assets/js/pages/uploader_bootstrap.js"></script>
+	<!-- /theme JS files -->
+
+</head>
+<body>
+    <form id="form1"  class="form-validate-jquery">
+        <div>
+            <!-- Main navbar -->
+            <div class="navbar navbar-inverse">
+                <div class="navbar-header">
+                    <a class="navbar-brand" href="index.html">
+                        <img src="assets/images/CCDO Logo.png" alt=""style="background-color:#ffffff"  /></a>
+
+                    <ul class="nav navbar-nav visible-xs-block">
+                        <li><a data-toggle="collapse" data-target="#navbar-mobile"><i class="icon-more"></i></a></li>
+                        <li><a class="sidebar-mobile-main-toggle"><i class="icon-paragraph-justify3"></i></a></li>
+                    </ul>
+                </div>
+
+                <div class="navbar-collapse collapse" id="navbar-mobile">
+                    <ul class="nav navbar-nav">
+                        <li><a class="sidebar-control sidebar-main-toggle hidden-xs"><i class="icon-paragraph-justify3"></i></a></li>
+                    </ul>
+
+                    <div class="navbar-right">
+                        <ul class="nav navbar-nav">
+                            <li>
+                                <a class="sidebar-control sidebar-opposite-fix hidden-xs" data-popup="tooltip" title="View Notification" data-placement="bottom" data-container="body">
+                                    <i class="icon-earth" style="padding-right: 10px;"></i>
+                                    <label ID="lblNotificationCounter" class="badge badge-primary position-left" Visible="false"></label>
+                                </a>
+                            </li>
+
+                            <li class="dropdown dropdown-user">
+                                <a class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="icon-cog"></i>
+                                    <span>
+                                        <label  ID="lblName" Text="Username"></label></span>
+                                    <i class="caret"></i>
+                                </a>
+
+                                <ul class="dropdown-menu dropdown-menu-right">
+                                    <li><a href="EditAccount.aspx"><i class="icon-cog5"></i>Account settings</a></li>
+                                    <li><a href="#"><i class="icon-switch2"></i> Logout</a></li>
+
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+
+                </div>
+            </div>
+            <!-- /main navbar -->
+
+            <!-- Page container -->
+            <div class="page-container">
+
+                <!-- Page content -->
+                <div class="page-content">
+
+                    <!-- Main sidebar -->
+                    <div class="sidebar sidebar-main">
+                        <div class="sidebar-content">
+
+                            <!-- User menu -->
+                            <div class="sidebar-user">
+                                <div class="category-content">
+                                    <div class="media">
+                                        <div class="media-left">
+                                            <img src="assets/images/CCDO Logo.png" class="img-circle img-sm" alt="" style="background-color:#ffffff"  />
+                                        </div>
+                                        <div class="media-body">
+                                            <span class="media-heading text-semibold">
+                                                <p  ID="txtUser" Text="Username"></p></span>
+                                            <div class="text-size-mini text-muted">
+                                                <i class="icon-pin text-size-small"></i>&nbsp;Santa Rosa, Laguna
+								
+                                       
+                                            </div>
+                                        </div>
+
+                                        <div class="media-right media-middle">
+                                            <ul class="icons-list">
+                                                <li>
+                                                    <a href="#"><i class="icon-cog3"></i></a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- /user menu -->
+                            
+                            <!-- Main Navigation -->
+                            <div class="sidebar-category sidebar-category-visible">
+                                <div class="category-content no-padding">
+                                    <ul class="navigation navigation-main navigation-accordion">
+
+                                        <li class="active">
+                                            <a href="#"><i class="icon-calendar"></i><span> Document</span></a>
+                                            <ul>
+                                                <li class="active"><a href="COOP_AddDocument.php">Add Document</a></li>
+                                                <li><a href="COOP_DocumentList.php">Documents List</a></li>
+                                            </ul>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                            </div>
+                            <!-- /Main Navigation -->
+
+                        </div>
+                    </div>
+                    <!--/ Main sidebar -->
+
+                    <!-- Main Content -->
+                    <div class="content-wrapper">
+                        <div class="content">
+
+                            <div class="panel panel-flat">
+                                <div class="panel-heading">
+                                    <div class="panel-title">
+                                        <h3 class="panel-title"><strong>Add Document</strong></h3>
+                                    </div>
+
+                                    <div class="heading-elements">
+                                        <div class="heading-btn-group">
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="panel-body">
+
+                                    <fieldset class="content-group">
+                                        <div class="col-lg-12">
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label">Tracking Number:</label>
+                                                    <div class="col-lg-12">
+                                                        <p class="label" ID="controlNumber1" Text="CCDO-0001" style="color: #000; font-size: 15px;">CCDO-0001</p>
+                                                    </div>
+                                                    
+                                                </div>
+                                            </div>
+
+                                            <div class="col-lg-6">
+                                                <div class="form-group">
+                                                    <label class="control-label"><span class="text-danger">* </span> <strong>Document Type: </strong></label>
+                                                    <select  class="form-control" required="required" ID="documentType">
+                                                        <option Value="0"></option>
+                                                        <option Value="1">Answer</option>
+                                                        <option Value="2">Post/Board</option>
+                                                        <option Value="3">Attend</option>
+                                                        <option Value="4">File</option>
+                                                        <option Value="5">Circulate</option>
+                                                        <option Value="6">Invitation</option>
+                                                    </select>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label">Administered by:</label>
+                                                    <input type="text"  class="form-control" Enabled="false" ID="Admin"></input>
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label class="control-label">Date Added:</label>
+                                                    <input type="text"  class="form-control" ID="dateTime" type="DateTime" Enabled="false"></input>
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-12">
+                                                <div class="form-group">
+                                                    <label><strong>Upload File:</strong></label>
+                                                    <asp:FileUpload  class="file-input-extensions" AllowMultiple="true" multiple="multiple" type="file" />
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="row">
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label><span class="text-danger">* </span><strong>Choose Cooperative Recipients:</strong></label>
+
+                                                    <div class="input-group content-group">
+                                                        <div class="has-feedback has-feedback-left">
+                                                            <asp:UpdatePanel >
+                                                                <ContentTemplate>
+                                                                    <input type="text"  class="form-control input-xs" ID="txtboxSearch" AutoPostBack="true"></input>
+
+                                                                </ContentTemplate>
+                                                            </asp:UpdatePanel>
+                                                            <div class="form-control-feedback">
+                                                                <i class="icon-search4 text-muted text-size-base"></i>
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="input-group-btn">
+                                                            <asp:Button  ID="btnSearchCooperative" class="btn btn-primary btn-xs" Text="Search" />
+                                                            <asp:Button  ID="btnSelectAllRecipients" class="btn bg-info btn-xs" Text="Select All" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <div class="col-lg-12">
+                                                            <asp:ListBox  ID="lbCooperative" Width="100%" Enabled="false" Height="300px" required="required"></asp:ListBox>
+
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+
+                                            <div class="col-md-6">
+                                                <div class="form-group">
+                                                    <label><span class="text-danger">* </span><strong>Choose Department Recipients:</strong></label>
+
+                                                    <div class="input-group content-group">
+                                                        <div class="has-feedback has-feedback-left">
+                                                            <asp:UpdatePanel >
+                                                                <ContentTemplate>
+                                                                    <input type="text"  class="form-control input-xs" ID="txtSearchDepartment" AutoPostBack="true"></input>
+                                                                </ContentTemplate>
+                                                            </asp:UpdatePanel>
+                                                            <div class="form-control-feedback">
+                                                                <i class="icon-search4 text-muted text-size-base"></i>
+                                                            </div>
+                                                        </div>
+
+                                                        <div class="input-group-btn">
+                                                            <asp:Button  ID="btnSearchDepartment" class="btn btn-primary btn-xs" Text="Search" />
+                                                            <asp:Button  ID="btnSelectAll" class="btn bg-info btn-xs" Text="Select All" />
+                                                        </div>
+                                                    </div>
+
+                                                    <div class="form-group">
+                                                        <div class="col-lg-12">
+                                                            <asp:ListBox  ID="lbDepartment" Width="100%" Enabled="false" Height="300px" required="required"></asp:ListBox>
+                                                        </div>
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        </div>
+                                    </fieldset>
+
+                                </div>
+
+                                <div class="panel-footer">
+                                    <div class="heading-elements">
+                                        <div class="text-right">
+                                            <input type="submit" ID="btnSend" text="Submit" class="btn bg-info" />
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>
+                    <!-- Main Content -->
+
+                </div>
+                <!-- /Page content -->
+
+            </div>
+            <!-- /Page container -->
+        </div>
+    </form>
+</body>
+</html>
