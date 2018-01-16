515eec658c618de931720791a8b3b89de6c2d671
Dherp Cuevas <34502161+dherpcuevas@users.noreply.github.com>
2018-01-16 20:45:35 +0800

    Revert "Merge branch 'master' of https://github.com/kabsnation/coop"
    This reverts commit 4aa10a32429b0802e7a62c53a5319257abc97149, reversing
    changes made to 91536c9437dcca26168cb8fd7f04f3d3cadb355f.

--- UI/index.php
+++ UI/index.php
@@ -1,8 +1,9 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head runat="server">
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>CCDO - Log in</title>
     <link rel="icon" href="assets/images/CCDO Logo.png" />
 
